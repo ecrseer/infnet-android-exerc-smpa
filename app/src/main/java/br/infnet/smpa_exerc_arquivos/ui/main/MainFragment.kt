181656764cc96bf55b4645800d499928223f4c32
ecrseer <juustino@live.com>
2022-02-15 22:07:48 -0300

    activty resultdd

--- app/src/main/java/br/infnet/smpa_exerc_arquivos/ui/main/MainFragment.kt
+++ app/src/main/java/br/infnet/smpa_exerc_arquivos/ui/main/MainFragment.kt
@@ -1,5 +1,6 @@
 package br.infnet.smpa_exerc_arquivos.ui.main
 
+import android.content.Intent
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -13,7 +14,7 @@ class MainFragment : Fragment() {
     companion object {
         fun newInstance() = MainFragment()
     }
-
+    val REQUEST_DIR =0
     private lateinit var viewModel: MainViewModel
 
     override fun onCreateView(
@@ -27,6 +28,13 @@ class MainFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode==REQUEST_DIR){
+
+        }
     }
 
 }
