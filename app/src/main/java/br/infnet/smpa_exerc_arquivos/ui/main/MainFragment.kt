43f965e2adb7249a94c43449bf57cc55ddb91580
ecrseer <juustino@live.com>
2022-02-15 22:20:54 -0300

    shows files names snackbarddd

--- app/src/main/java/br/infnet/smpa_exerc_arquivos/ui/main/MainFragment.kt
+++ app/src/main/java/br/infnet/smpa_exerc_arquivos/ui/main/MainFragment.kt
@@ -1,20 +1,29 @@
 package br.infnet.smpa_exerc_arquivos.ui.main
 
+import android.app.Activity.RESULT_OK
 import android.content.Intent
+import android.net.Uri
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.documentfile.provider.DocumentFile
 import br.infnet.smpa_exerc_arquivos.R
+import com.google.android.material.snackbar.Snackbar
+import java.io.File
 
 class MainFragment : Fragment() {
 
     companion object {
         fun newInstance() = MainFragment()
     }
-    val REQUEST_DIR =0
+
+    val REQUEST_DIR = 99
     private lateinit var viewModel: MainViewModel
 
     override fun onCreateView(
@@ -30,10 +39,23 @@ class MainFragment : Fragment() {
         // TODO: Use the ViewModel
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<Button>(R.id.btn_call_open_file).setOnClickListener {
+            startActivityForResult(Intent(Intent.ACTION_OPEN_DOCUMENT_TREE), REQUEST_DIR)
+        }
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode==REQUEST_DIR){
-
+        if (requestCode == REQUEST_DIR && resultCode == RESULT_OK) {
+            val uri: Uri = data?.data!!
+            val directory = DocumentFile.fromTreeUri(requireContext(),uri)
+            //val f:File = File(uri)
+            for(file in directory!!.listFiles()){
+                val view = requireActivity().findViewById<TextView>(R.id.message)
+                Snackbar.make(view,"${file.name}", Toast.LENGTH_LONG+4242).show()
+            }
         }
     }
 
